b10da79377ae86394a0bb468f9eae49d68687992
irissz <tissinaira@gmail.com>
2025-12-03 19:30:42 +0500

    comit1

--- script.js
+++ script.js
@@ -1,7 +1,36 @@
 let textClose = 'Скрыть';
-document.querySelector('.button').addEventListener('click', function() {
+document.querySelector('.button').addEventListener('click', function () {
     document.querySelector('.info__text').classList.toggle('info__text--active');
     let textOpen = this.textContent;
     this.textContent = textClose;
     textClose = textOpen;
+});
+
+
+const popup = document.querySelector('.popup');
+
+document.addEventListener('DOMContentLoaded', function() {
+    const PopupShow = document.querySelector('.show-popup');
+    if (PopupShow) {
+        PopupShow.addEventListener('click', function() {
+            if (popup) {
+                document.body.classList.add('no-scroll');
+                popup.classList.add('popup--show');
+                setTimeout(function() {
+                    popup.classList.add('popup--active');
+                }, 10);
+            }
+        });
+    }
+
+    const PopupClose = document.querySelector('.popup__close');
+    if (PopupClose) {
+        PopupClose.addEventListener('click', function () {
+            popup.classList.remove('popup--active');
+            setTimeout(function() {
+                popup.classList.remove('popup--show');
+                document.body.classList.remove('no-scroll');
+            }, 300);
+        });
+    }
 });
